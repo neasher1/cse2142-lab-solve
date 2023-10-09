784cf24dcb12aaea91b860c4da98a29486ac96c3
neasher1 <neasher75@gmail.com>
2023-10-09 16:29:59 +0600

    STEP #2.    Application now terminates early if the number of arguments passed into it is wrong, fix it

--- StudentList.java
+++ StudentList.java
@@ -5,6 +5,11 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
+		if (args == null || args[0].length() != 1) {
+			System.out.println("Please provide a, r, ?, +, or c argument");
+			return;
+		}
+
 		// Check arguments
 		if (args[0].equals("a")) {
 
